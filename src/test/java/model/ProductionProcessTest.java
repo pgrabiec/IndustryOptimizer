25f0f464996e2edcbb60e64ca820bdbe81408f9f
Piotr Grabiec <pgrabiec7@gmail.com>
2017-05-09 12:29:27 +0200

    Added JUnit to dependencies

---
+++ src/test/java/model/ProductionProcessTest.java
@@ -0,0 +1,23 @@
+package model;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+@RunWith(Arquillian.class)
+public class ProductionProcessTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
